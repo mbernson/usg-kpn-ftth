e2acb5797cb71ca5cfbb9eb18df0201b5197572f
Henk van Achterberg <coolhva@gmail.com>
2020-03-14 14:30:06 +0100

    Update setvpn.sh
    save not needed, this script is run after reboot

--- setvpn.sh
+++ setvpn.sh
@@ -10,5 +10,4 @@ set vpn ipsec ipsec-interfaces interface pppoe2 >> ${logFile}
 delete vpn l2tp remote-access dhcp-interface eth0 >> ${logFile}
 set vpn l2tp remote-access outside-address 0.0.0.0 >> ${logFile}
 commit
-save
 exit
